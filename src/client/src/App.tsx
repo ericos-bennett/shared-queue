bac782b3ad0ec776a562a95fabb8ebf1712e9a6e
Eric <ericos.bennett@gmail.com>
2021-03-28 22:23:05 -0700

    testing for client credentials flow

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,34 +1,14 @@
 import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
 
-const hitBackend = () => {
-  axios.get('/test')
-  .then((response) => {
-  console.log(response.data)
-  })
-  }
+const hitBackend = async () => {
+  const res = await axios.get('/test')
+  console.log(res.data);
+}
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={hitBackend}>Send request</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={hitBackend}>Send request</button>
     </div>
   );
 }
-
-export default App;
