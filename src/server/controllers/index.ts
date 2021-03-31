7ed278ca71537b23eb0d9d7b4ac173d85efad04b
Eric <ericos.bennett@gmail.com>
2021-03-31 10:19:38 -0700

    send across access + refresh tokens as cookies

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,16 +1,21 @@
 import { Request, Response } from 'express';
 import { getAuthUrl, setAuthTokens } from '../services';
 
-const getAuthCode = (req: Request, res: Response) => {
+const getAuthCode = (req: Request, res: Response): void => {
   const authUrl: string = getAuthUrl();
   res.send(authUrl);
 };
 
 const getAuthTokens = async (req: Request, res: Response) => {
-  const { code, state } = req.query;
-  console.log('Code: ', code);
-  await setAuthTokens(code);
-  res.send('You made it!');
+  const code = req.query.code as string;
+  // const state = req.query.state as string;
+
+  const { userId, accessToken, refreshToken } = await setAuthTokens(code);
+  
+  res.cookie('userId', userId);
+  res.cookie('accessToken', accessToken);
+  res.cookie('refreshToken', refreshToken);
+  res.redirect('http://localhost:3000');
 };
 
 export { getAuthCode, getAuthTokens };
--- src/server/services/auth.ts
+++ src/server/services/auth.ts
@@ -2,12 +2,12 @@ import SpotifyWebApi from 'spotify-web-api-node';
 
 const getAuthUrl = (): string => {
 
-  const scopes: string[] = ['playlist-modify-private'];
-  const state: string = 'some-state-of-my-choice';
-  
   const clientId: string = process.env.CLIENT_ID!;
   const clientSecret: string = process.env.CLIENT_SECRET!;
   const redirectUri: string = 'http://localhost:8080/api/auth/token';
+
+  const scopes: string[] = ['playlist-modify-private'];
+  const state: string = 'some-state-of-my-choice';
   
   const spotifyApi = new SpotifyWebApi({
     clientId,
@@ -24,23 +24,27 @@ const setAuthTokens = async (code: string) => {
 
   const clientId: string = process.env.CLIENT_ID!;
   const clientSecret: string = process.env.CLIENT_SECRET!;
-  const redirectUri: string = 'http://localhost:3000';
+  const redirectUri: string = 'http://localhost:8080/api/auth/token';
 
   const spotifyApi = new SpotifyWebApi({
     clientId,
     clientSecret,
-    redirectUri
+    redirectUri,
   });
 
   const response = await spotifyApi.authorizationCodeGrant(code)
+  const accessToken = response.body['access_token'];
+  const refreshToken = response.body['refresh_token'];
 
-  console.log(response.body);
+  // Set the access token on the API object to use it in the user call
+  spotifyApi.setAccessToken(accessToken);
+  spotifyApi.setRefreshToken(refreshToken);
   
-  // Set the access token on the API object to use it in later calls
-  spotifyApi.setAccessToken(response.body['access_token']);
-  spotifyApi.setRefreshToken(response.body['refresh_token']);
+  const user = await spotifyApi.getMe();
+  const userId = user.body['id'];
+  
+  return { userId, accessToken, refreshToken };
 
 };
-
 
 export { getAuthUrl, setAuthTokens };
