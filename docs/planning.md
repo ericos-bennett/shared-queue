3660c74fc2b0ac7c3316d2a484fe3c00256c5745
Eric <ericos.bennett@gmail.com>
2021-03-31 15:52:53 -0700

    add components for landing page

--- docs/planning.md
+++ docs/planning.md
@@ -1,8 +1,18 @@
 # Planning
 
 ### Learning Objectives
-- TypeScript (back + front)
-- Express
-- Async Await
+- TypeScript
+- React Router
 - Spotify API
-- OAuth + 3rd Party Sign Ins
+- OAuth2
+- Async Await
+
+### Next Steps
+- Build out home page components
+- Use react router to route to new room with playlist ID when you create one
+- Build out listening room components
+  - User can add tracks to playlist from app
+  - User can pause + start playback from app
+- Other users can authenticate and join a room (with its id)
+  - Other users pause + stop playback from app
+  - Other users can add tracks to playlist from app
--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,21 +1,84 @@
 import axios from 'axios';
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Typography } from '@material-ui/core';
+
 
 const userAuthRedirect = async () => {
   const res = await axios.get('/api/auth/code');
   window.location.href = res.data;
 }
 
-const createPlaylist = async () => {
-  const res = await axios.post('/api/playlist', {name: 'abc'});
-  console.log(res);
-}
+// const createPlaylist = async () => {
+//   const res = await axios.post('/api/playlist', {name: 'abc'});
+//   console.log(res)
+// }
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100vw',
+    height: '100vh',
+    overflow: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundImage: 'linear-gradient(90deg, #2c5e92 0%, #552f6d 80%)'
+
+  },
+  banner: {
+    width: '60%',
+    color: 'white',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  description: {
+    paddingTop: '40px',
+    textAlign: 'center'
+  },
+  buttons: {
+    width: '60%',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  button: {
+    width: '25%',
+    height: '150%',
+    color: 'white',
+    borderColor: 'white'
+  }
+}));
 
 export default function App() {
-  return (
-    <div className="App">
-      <button onClick={userAuthRedirect}>Sign In</button>
-      <button onClick={createPlaylist}>Create Playlist</button>
 
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.banner}>
+        <Typography variant="h2">
+          Welcome to Spotify Mix!
+        </Typography>
+        <Typography variant="h4" className={classes.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </Typography>
+      </div>
+      <div className={classes.buttons}>
+        <Button
+          className={classes.button}
+          variant="outlined"
+          onClick={userAuthRedirect}
+        >
+          New Mix
+        </Button>
+        <Button
+          className={classes.button}
+          variant="outlined"
+          onClick={() => console.log('implement me!')}
+        >
+          Tune In
+        </Button>
+      </div>
     </div>
   );
 }
