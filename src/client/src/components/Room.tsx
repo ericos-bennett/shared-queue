006529088e6a6caaeeebbe41a494b0f09422c0f1
Eric <ericos.bennett@gmail.com>
2021-04-02 15:57:46 -0700

    websockets setup

--- src/client/src/components/Room.tsx
+++ src/client/src/components/Room.tsx
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router'
+import io from "socket.io-client";
+
+const ENDPOINT = 'http://localhost:3000'
 
 export default function Room() {
+
+  // const [response, setResponse] = useState("");
+  
+  useEffect(() => {
+    const socket = io(ENDPOINT);
+    socket.emit('message', 'client says hi!');
+  }, []);
 
   let { id } = useParams<{ id: string }>();
 
