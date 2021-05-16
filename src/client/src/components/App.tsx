3e3d06cb9891d488d80f00a98e630dca5d8514b3
Rex <you@example.com>
2021-05-16 15:02:34 +1000

    Wrapped login response from Spotify in useCallback

--- src/client/src/components/App.tsx
+++ src/client/src/components/App.tsx
@@ -1,7 +1,7 @@
 import { useReducer, useMemo, useEffect, useCallback } from 'react';
-import {Context, initialState} from '../reducers/context';
+import { Context, initialState } from '../reducers/context';
 import appActions from '../actions/appActions';
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './Home';
 import Room from './Room';
@@ -10,23 +10,24 @@ import Cookie from 'js-cookie';
 
 export default function App() {
   const [state, dispatch] = useReducer(playerReducer, initialState)
-  const login_res = (e: any) => {
-  // TODO: surely theres a better way
-    console.log('e', e)
 
-  const location = e.location
+  const login_res = useCallback((e: any) => {
+    console.info("login_res")
+    // TODO: surely there is a better way
+    const location = e.location
 
-    const login_result = location.search.substring(0,6) === '?code=' 
+    const login_result = location.search.substring(0, 6) === '?code='
 
-    const code = login_result && location.search.substring(6,location.search.length)
+    const code = login_result && location.search.substring(6, location.search.length)
 
-    appActions.updateLoginStatus(state, dispatch, {login_result, code})
+    appActions.updateLoginStatus(state, dispatch, { login_result, code })
     return <Redirect to="/" />
-    
-  }
+
+  }, [state])
 
   const setIsAuthenticated = useCallback(
     () => {
+      console.info("setIsAuthenticated")
       Cookie.get('accessToken') && !state.logged_in && appActions.setLoginStatus(state, dispatch, true)
     },
     [state, dispatch],
@@ -37,32 +38,32 @@ export default function App() {
     setIsAuthenticated()
   }, [setIsAuthenticated])
 
-    // create object to be passed as value, using memo to encapsulate against unnecessary updates
-    const ContextValue = useMemo(() => {
-      return {
-        state,
-        dispatch
-      }
+  // create object to be passed as value, using memo to encapsulate against unnecessary updates
+  const ContextValue = useMemo(() => {
+    return {
+      state,
+      dispatch
+    }
   }, [state, dispatch])
 
   return (
     <Context.Provider value={ContextValue} >
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route
-          path="/room/:id"
-          render={() => (Cookie.get('accessToken') ? <Room /> : <Redirect to="/" />)}
-        />
-        <Route
-          path="/login_res"
-          render={login_res}
-        />        
-        <Redirect from="*" to="/" />
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route
+            path="/room/:id"
+            render={() => (Cookie.get('accessToken') ? <Room /> : <Redirect to="/" />)}
+          />
+          <Route
+            path="/login_res"
+            render={login_res}
+          />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Router>
     </Context.Provider>
   );
 }
