340294ec42d9fbc03866f4d863603ff784aa131b
Rex <you@example.com>
2021-05-19 09:51:31 +1000

    Updated queue generation to use playlist

--- src/client/src/components/Queue.tsx
+++ src/client/src/components/Queue.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Context } from '../reducers/context';
 import DeleteButton from './DeleteButton';
@@ -27,8 +27,40 @@ const useStyles = makeStyles(() => ({
 export default function Queue() {
   const classes = useStyles();
   const { state } = useContext(Context);
+  const { spotifyApi } = state
+  const [playlist, setPlaylist] = useState([])
 
-  const listItems = state?.tracks.map((track: any, i: number) => {
+  const updateQueue = useCallback(
+    () => {
+      // Get a playlist
+      spotifyApi && spotifyApi.getPlaylist(state.playlistId)
+        .then(function (data: any) {
+          console.log('data :>> ', data);
+          setPlaylist(data.body.tracks.items.map((track: any) => {
+            return {
+              artist: track.track.artists[0].name,
+              title: track.track.name,
+              id: track.track.id,
+              albumUrl: track.track.album.images[2].url,
+              durationMs: track.track.duration_ms,
+            };
+          }
+          ))
+          // setTracks()
+          console.log('Some information about this playlist', data.body);
+        }, function (err: any) {
+          console.log('Something went wrong!', err);
+        });
+    },
+    [spotifyApi, state.playlistId],
+  )
+
+  useEffect(() => {
+    console.log('updateQueue [useEffect]');
+    updateQueue()
+  }, [state.tracks, updateQueue])
+
+  const listItems = playlist!.map((track: any, i: number) => {
     return (
       <li className={classes.track} key={i}>
         <img src={track.albumUrl} alt={track.artist}></img>
@@ -42,7 +74,7 @@ export default function Queue() {
 
   return (
     <ul className={classes.root}>
-      {state?.tracks.length ? (
+      {playlist.length ? (
         listItems
       ) : (
         <p className={classes.emptyTracks}>Add tracks above to get started!</p>
