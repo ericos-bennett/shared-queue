ba14562babe1b3b70e94c5f5cbfb6e21ae18b7e0
Rex <you@example.com>
2021-05-19 09:54:09 +1000

    Add scope for Spotify SDK auth

--- src/server/services/auth.ts
+++ src/server/services/auth.ts
@@ -3,10 +3,15 @@ import { createSpotifyApi } from '../utils';
 const getAuthUrl = (): string => {
   const spotifyApi = createSpotifyApi();
 
+  // https://developer.spotify.com/documentation/general/guides/scopes/
   const scopes: string[] = [
     'streaming', // For the Web Playback SDK
     'user-read-email', // For the Web Playback SDK
     'user-read-private', // For the Web Playback SDK
+    'user-read-playback-state', // Get the User's Currently Playing Track
+    "playlist-read-private", "playlist-modify-private", "playlist-modify-public"
+
+
   ];
   const state = 'some-state-of-my-choice'; // Implement security here
 
