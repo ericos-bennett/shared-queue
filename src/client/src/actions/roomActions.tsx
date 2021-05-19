7ed2192649114c9a04fbc1ed395e397fd4bc1314
Rex <you@example.com>
2021-05-20 09:34:03 +1000

    Created Exit Room Action

--- src/client/src/actions/roomActions.tsx
+++ src/client/src/actions/roomActions.tsx
@@ -1,4 +1,5 @@
 import { types } from '../reducers/actionTypes';
+import { playerActions } from './playerActions';
 
 const setRoomId = (dispatch: any, payload: any) => {
   console.info('setRoomId');
@@ -8,9 +9,22 @@ const setRoomId = (dispatch: any, payload: any) => {
   });
 };
 
+// If you're the first person in the room, make a new one...
+// { 2, 3, 4 }
+// When you join the room, ask the 'host' for the current state
+// 4 joins, 2 sends them the tracklist and current playtime(ms) for them to sync with
+
+const exitRoom = (state: any, dispatch: any) => {
+  console.info('exitRoom');
+  playerActions.pause(state, dispatch)
+  dispatch({
+    type: types.EXIT_ROOM
+  });
+};
+
 
 
 export const roomActions = {
   setRoomId,
-
+  exitRoom
 };
