aae8b41deaa6231682fa0dc790e87b9251352585
Rex <rex.brooker@gmail.com>
2021-05-28 18:53:54 +1000

    make it better

--- src/client/src/services/spotifyApi.ts
+++ src/client/src/services/spotifyApi.ts
@@ -3,7 +3,7 @@ import SpotifyWebApi from './spotify-web-api-node-pkce/server';
 const createSpotifyApi = (): SpotifyWebApi => {
 
   // TODO 
-  const clientId = '0e4fbd47ca0c471f83fa9732a77ad6c9'//process.env.CLIENT_ID;
+  const clientId = '0e4fbd47ca0c471f83fa9732a77ad6c9' //process.env.CLIENT_ID;
   const redirectUri = 'http://localhost:3000/api/auth/token'//process.env.AUTH_REDIRECT_URI;
   return new SpotifyWebApi({
     clientId,
